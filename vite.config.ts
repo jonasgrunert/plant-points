15fb9e9b8408528845b6b47fcc4af39142c1043d
Jonas Grunert <jonas.grunert@bayer.com>
2025-10-28 14:16:46 +0100

    Make inject register

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: false,
+      injectRegister: "auto",
 
       pwaAssets: {
         disabled: false,
