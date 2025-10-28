9ec928da896d39ace87e6f07a566ea527221d388
Jonas Grunert <jonas.grunert@bayer.com>
2025-10-28 13:43:40 +0100

    Adding install prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 //import PWABadge from "./pwa/PWABadge.tsx";
 import { route as layout } from "./routes/layout.tsx";
+import { useEffect, useState } from "react";
+import { Button } from "react-aria-components";
+import { useTranslation } from "react-i18next";
+
+type PromtFn = () => Promise<{ outcome: "accepted" | "dismissed" }>;
 
 const router = createBrowserRouter([layout]);
 
 function App() {
+  const [install, setInstall] = useState<PromtFn | null>(null);
+  useEffect(() => {
+    const onBefore = (
+      event: Event & {
+        prompt?: PromtFn;
+      }
+    ) => {
+      if (event.prompt) {
+        setInstall(event.prompt);
+      }
+    };
+
+    window.addEventListener("beforinstallprompt", onBefore);
+
+    return () => {
+      window.removeEventListener("beforinstallprompt", onBefore);
+    };
+  }, []);
+  const { t } = useTranslation();
   return (
     <>
       <RouterProvider router={router} />
       {/* <PWABadge /> */}
+      {install !== null && (
+        <div className="install-button">
+          <Button className="outline">{t("install")}</Button>
+        </div>
+      )}
     </>
   );
 }
